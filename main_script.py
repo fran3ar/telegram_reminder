b1fd8e36e7127f8475631df7a18ac9914b1e4f4a
fran3ar <fran3ar@gmail.com>
2025-12-12 13:19:20 -0300

    Refactor main_script.py to clean up code and use env vars
    Removed unused imports and commented-out code for clarity. Updated to use environment variables for CHAT_ID and MESSAGE.

---
+++ main_script.py
@@ -0,0 +1,18 @@
+import requests
+from datetime import datetime
+
+def send_telegram_message(token, chat_id, text):
+    try:
+        url = f"https://api.telegram.org/bot{token}/sendMessage"
+        payload = {"chat_id": chat_id, "text": text}
+        response = requests.post(url, data=payload)
+        return response.json()
+    except Exception as e:
+        print("⚠️ Error enviando mensaje a Telegram:", e)
+        return None
+
+BOT_TOKEN = os.getenv("SECRET_BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+MESSAGE = os.getenv("MESSAGE")
+
+send_telegram_message(BOT_TOKEN, CHAT_ID, MESSAGE)
