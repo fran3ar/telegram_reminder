b02aa039d21060d1016e7ac0173a477599690777
Francisco Oviedo <fran3ar@gmail.com>
2025-12-21 20:16:29 -0300

    yyyyyy

--- main_script.py
+++ main_script.py
@@ -1,19 +1,125 @@
-import os
-import requests
-from datetime import datetime
+import warnings
+warnings.filterwarnings('ignore')
 
-def send_telegram_message(token, chat_id, text):
+#%%
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+# Cargar variables desde .env
+load_dotenv()
+
+DB_URL = os.getenv("DB_URL")
+
+#######
+# --- EMERGENCY RESET BUTTON ---
+
+try:
+    conn = psycopg2.connect(DB_URL)
+    conn.autocommit = True
+    with conn.cursor() as cur:
+        kill_query = """
+        SELECT pg_terminate_backend(pid)
+        FROM pg_stat_activity
+        WHERE datname = 'MyDatabase1_pickthrew'
+            AND pid <> pg_backend_pid();
+        """
+        cur.execute(kill_query)
+        print("All other connections closed!")
+    conn.close()
+except Exception as e:
+    print(f"Could not reset: {e}")
+########
+
+conn = psycopg2.connect(DB_URL)
+
+
+#%%
+import pandas as pd
+
+def display_table(query):
+    cursor = conn.cursor()
+    try:
+        query = query
+        df = pd.read_sql(query, conn)
+        return df
+    except Exception as e:
+        print("Error al mostrar la tabla:", e)
+        return None
+    finally:
+        cursor.close()
+        # NO cerramos conn aquí
+
+#%%
+
+data1 = display_table("""                                        
+SELECT *
+FROM my_schema_1.reminders
+WHERE activated = TRUE
+"""
+)
+
+# Cerrar conexión al final
+conn.close()
+
+#%%
+
+import requests
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+def send_telegram_message(token, chat_id, text_msg):
     try:
         url = f"https://api.telegram.org/bot{token}/sendMessage"
-        payload = {"chat_id": chat_id, "text": text}
+        payload = {"chat_id": chat_id, "text": text_msg}
         response = requests.post(url, data=payload)
         return response.json()
     except Exception as e:
-        print("⚠️ Error enviando mensaje a Telegram:", e)
+        print(f"⚠️ Error: {e}")
         return None
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-MESSAGE = os.getenv("MESSAGE")
+#%%
 
-send_telegram_message(BOT_TOKEN, CHAT_ID, MESSAGE)
+from datetime import datetime
+import time
+now = datetime.now()
+
+now_yyyymm = now.strftime("%Y-%m")
+
+print(now_yyyymm)
+
+
+#%%
+
+
+for _, row in data1.iterrows():
+    should_send = False
+    frequency = row['frequency']
+
+    day_of_week = row['day_of_week']
+    day_value = row['day_value']
+    month_value = row['month_value']
+    year_value = row['year_value']
+    hour_value = row['hour_value']
+    minute_value = row['minute_value']
+    last_completed_at = row['last_completed_at']
+    print(last_completed_at)
+    
+    last_completed_at_yyyymm = last_completed_at.strftime("%Y-%m")
+
+    chat_id = row['chat_id']
+    reminder = row['reminder']
+    print(f"frequency: {frequency}")
+    print(f"id: {row['id']}")
+
+    if frequency == "monthly" and round(day_value) <= now.day and last_completed_at_yyyymm != now_yyyymm:
+        # print(round(day_value))
+        # print(now.day)
+        # print(last_completed_at.month)
+        # print(now.month)
+        should_send = True
+    
+    if should_send:
+        send_telegram_message(BOT_TOKEN, chat_id, reminder)
+        print("Sent")
+        time.sleep(3)  # delays for 3 seconds
